95913fb2bf2bf919fd5573fdc330df61a1b2bb4a
MyrPasko <myroslavpasko@gmail.com>
2019-02-12 06:52:36 +0200

    # On lesson 6 19.00

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const bodyParser = require('body-parser');
 const graphqlHttp = require('express-graphql');
 const {buildSchema} = require('graphql');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
-
-/** Temporary solution */
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -15,7 +16,9 @@ app.use(bodyParser.json());
  * Something! - This must never be null!
  * 'events' can take arguments too.
  * ID - special type, must be unique, GQL add it by itself
- * 'input' - special type for inputs(properties of data object)*/
+ * 'input' - special type for inputs(properties of data object)
+ * Check the password field in User schema. It must be nullable
+ * Check the password field in UserInput schema. It must not be nullable*/
 
 app.use('/graphql', graphqlHttp({
     schema: buildSchema(`
@@ -27,11 +30,22 @@ app.use('/graphql', graphqlHttp({
             date: String!
         }
         
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+        
         input EventInput {
             title: String!
             description: String!
             price: Float!
             date: String!
+        }
+        
+        input UserInput {
+            email: String!
+            password: String!
         }
         
         type RootQuery {
@@ -40,6 +54,7 @@ app.use('/graphql', graphqlHttp({
         
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         
         schema {
@@ -48,31 +63,99 @@ app.use('/graphql', graphqlHttp({
         }
     `),
     rootValue: {                     // all schemas, matching by names
+
+        /** Events resolvers */
         events: () => {              // resolver, handle data, if it has form 'events' from schema
-            return events;
+            return Event
+                .find()
+                .then((events) => {
+                    return events.map((event) => {
+                        console.log({...event._doc});
+                        return {
+                            ...event._doc,
+                            _id: event._doc._id.toString()
+                        };
+                    })
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         },
         createEvent: (args) => {
             const {title, description, price, date} = args.eventInput;  // there is nested object of type EventInput with the name "eventInput"
-            const event = {
-                _id: Math.random().toString(),
+            // Hardcode method
+            // const event = {
+            //     _id: Math.random().toString(),
+            //     title: title,
+            //     description: description,
+            //     price: +price,
+            //     date: date
+            // };
+
+            const event = new Event({
                 title: title,
                 description: description,
                 price: +price,
-                date: date
-            };
-            console.log({event, events});
-            events.push(event);
-            console.log({events});
+                date: new Date(date)
+            });
 
-            return event;                                               // new item must be return
-        }
+            return event
+                .save()                                // new item must be return
+                .then((result) => {
+                    console.log(result);
+                    return {
+                        ...result._doc,
+                        _id: result._doc._id.toString()
+                    };
+                })
+                .catch((err) => {
+                    console.log(err);
+                    throw err;
+                });
+
+        },
+
+        /** User resolvers */
+        createUser: (args) => {
+            const {email, password} = args.userInput;  // there is nested object of type EventInput with the name "eventInput"
+
+            return User.findOne({email: email})
+                .then((user) => {
+                    if (user) {
+                        throw new Error('User exists already.')
+                    }
+                    return bcrypt.hash(password, 12)
+                })
+                .then((hashedPassword) => {
+                    const user = new User({
+                        email: email,
+                        // password: password                     // it is wrong way of storing data, we can't store password as plain text
+                        password: hashedPassword
+                    });
+
+                    return user.save()                                // new item must be return
+                })
+                .then((result) => {
+                    console.log(result);
+                    return {
+                        ...result._doc,
+                        password: null,
+                        _id: result._doc._id.toString()
+                    };
+                })
+                .catch((err) => {
+                    throw err;
+                });
+
+
+        },
     },
     graphiql: true                   // little text redactor for queries
 }));
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${
     process.env.MONGO_PASSWORD
-    }@graphql-learn-cluster-juk75.mongodb.net/test?retryWrites=true`)
+    }@graphql-learn-cluster-juk75.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
     .then(() => {
         app.listen(3000);
     })
